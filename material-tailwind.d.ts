08eeddd60a3f861eb379938c2f99758a605d9f63
bhagirath-bhp <bhagirath.bhp@gmail.com>
2024-04-13 10:20:08 +0530

    Fixed TS errors-5

--- material-tailwind.d.ts
+++ material-tailwind.d.ts
@@ -1,12 +1,9 @@
-declare module "@material-tailwind/react/*";
-
-declare module "@material-tailwind/react" {
+declare module "@material-tailwind/react/*" {
   interface IconButtonProps extends React.ComponentProps<"button"> {
     children?;
   }
   interface DialogProps extends React.ComponentProps<"div"> {
     handler?;
-    children?;
   }
   interface ButtonProps extends React.ComponentProps<"button">  {
     children?;
